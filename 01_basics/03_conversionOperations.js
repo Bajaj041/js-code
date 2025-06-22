83185541778168f25c7815b167214220379c61bc
Aditya Bajaj <141822662+Bajaj041@users.noreply.github.com>
2025-06-22 08:52:14 +0000

    operations done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,4 @@
+/* Conversions
 let score = true
 
 console.log(typeof score);
@@ -11,3 +12,24 @@ let stringNumber = String(33);
 
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+*/
+
+// Operations
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Aditya";
+let str2 = " Bajaj";
+let str3 = str1 + str2;
+// console.log(str1 + str2);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(typeof (+true));
