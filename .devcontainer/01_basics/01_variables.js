8afa20ffa6a0c70d263e8de0b230b5cf5b51d332
Aditya Bajaj <141822662+Bajaj041@users.noreply.github.com>
2025-06-22 07:48:44 +0000

    container done config

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "aditya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ghaziabad"
+
+accountEmail = "sahdkjhskj@gmail.com"
+accountPassword = "121212"
+accountCity = "aligarh"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+// number -> 2 to the power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol -> unique identification
+
+console.log(typeof undefined);
+console.log(typeof null);
