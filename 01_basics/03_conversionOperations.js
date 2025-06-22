feef635755731bafe2529b1e35c1a32cefd8c4ad
Aditya Bajaj <141822662+Bajaj041@users.noreply.github.com>
2025-06-22 08:35:14 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = true
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let someNumber = 33;
+let stringNumber = String(33);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
