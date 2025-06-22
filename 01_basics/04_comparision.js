a01f8263442cd2c9fc06a0d8473ee213761bf7a5
Aditya Bajaj <141822662+Bajaj041@users.noreply.github.com>
2025-06-22 09:02:26 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// undefined gives "FALSE" with every comparision
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//Strict Check '===', even checks the dataType
+console.log("2" === 2);
