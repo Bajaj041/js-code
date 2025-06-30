ad130e98847406dbc24118c5eed3811e4c1a72f6
Aditya Bajaj <141822662+Bajaj041@users.noreply.github.com>
2025-06-30 11:15:57 +0000

    date and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+// Date
+
+let myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+let today = new Date(2025, 5, 30);
+console.log(today.toDateString);
